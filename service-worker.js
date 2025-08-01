be31ca7d6bf1de69ac969230ff843564d5ccffd9
gamestudioz-in <arealgmr@gmail.com>
2025-08-01 11:37:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'my-site-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'index.html',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
 ];
 
 // Install the service worker
