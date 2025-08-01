ebd936a9b6d409e7c7c0233a4fec6e2f3dcd8603
gamestudioz-in <arealgmr@gmail.com>
2025-08-01 12:18:52 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'vaulta-password-generator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon192.png',
+  '/icon512.png',
+  // Add any other assets you want to cache offline
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
